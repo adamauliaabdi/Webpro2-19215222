c7ac611c6c014cc7b2598059103a7649584ec532
adam aulia abdi <128812307+adamauliaabdi@users.noreply.github.com>
2023-05-07 14:29:29 +0700

    Add files via upload

---
+++ V_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="circle">
+ <li>Nama Depan : Adam </li>
+ <li>Nama Belakang : Aulia Abdi</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="circle">
+ <li> Jl Menteng Atas</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="circle">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="circle">
+ <li>Lukis</li>
+ <li>Running</li>
+ </ul>
+</section>
